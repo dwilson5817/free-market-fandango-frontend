135f726952b5694555812ef699e39bfd97378e86
Dylan Wilson <dwilson5817@gmail.com>
2023-08-24 02:00:57 +0100

    Add recent transactions to check card page
    Show the users recent transactions on the check card page.

--- src/components/pages/ViewCardPage/AccountPageAccountInfo.vue
+++ src/components/pages/ViewCardPage/AccountPageAccountInfo.vue
@@ -2,10 +2,12 @@
 import {defineComponent} from 'vue'
 import VAlertMessage from "@/components/VAlertMessage.vue";
 import AccountService from "@/services/account.service";
+import AppCard from "@/components/AppCard.vue";
+import {formatDistance} from "date-fns";
 
 export default defineComponent({
   name: "AccountPageAccountInfo",
-  components: {VAlertMessage},
+  components: {AppCard, VAlertMessage},
   data() {
     return {
       cardNumber: null,
@@ -38,6 +40,9 @@ export default defineComponent({
           console.log(this.errorMessage)
         }
       );
+    },
+    timeSince(datetime) {
+      return formatDistance(new Date(datetime), new Date(), { addSuffix: true })
     }
   },
 })
@@ -48,44 +53,81 @@ export default defineComponent({
     name="slide-left"
     mode="out-in"
   >
-    <div
-      v-if="account"
-      class="w-full mx-auto mb-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-    >
-      <div class="flex justify-end px-4 pt-4">
-        <button
+    <div v-if="account">
+      <button
           type="button"
-          class="inline-block focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+          class="inline-block focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
           @click="account = null"
-        >
-          Back
-        </button>
-      </div>
-      <div
-        class="flex flex-col items-center pb-10"
       >
-        <svg
-          aria-hidden="true"
-          class="w-24 h-24 mb-3 rounded-full shadow-lg text-white"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            clip-rule="evenodd"
-            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z"
-            fill-rule="evenodd"
-          />
-        </svg>
-        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-          {{ account.name }}
-        </h5>
-        <span class="text-sm text-gray-500 dark:text-gray-400">Card number {{ account.card_number }}</span>
+        Back
+      </button>
 
-        <div class="flex mt-4 space-x-3 md:mt-6">
-          <span class="bg-blue-100 text-blue-800 text-sm font-medium px-5 py-2.5 rounded dark:bg-blue-900 dark:text-blue-300">Balance: ${{ account.balance }}</span>
-        </div>
-      </div>
+      <AppCard class="my-4">
+        <template #title>
+          <h1 class="mb-3 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Hello, {{ account.name }}!
+          </h1>
+        </template>
+
+        <template #body>
+          <p class="font-normal text-2xl text-gray-700 dark:text-gray-400">
+            Your current balance is <strong>{{ account.balance }} Edinbucks</strong>.
+          </p>
+        </template>
+      </AppCard>
+      <AppCard>
+        <template #svg>
+          <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+        </template>
+
+        <template #title>
+          Purchases
+        </template>
+
+        <template #body>
+          <table class="w-full mt-4 text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" class="px-6 py-3">
+                Stock code
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Stock name
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Price
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Time
+              </th>
+            </tr>
+            </thead>
+            <tbody v-if="account.purchases.length === 0">
+            <tr class="bg-white dark:bg-gray-900 dark:border-gray-700">
+              <th scope="row" colspan="4" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                No purchases found!
+              </th>
+            </tr>
+            </tbody>
+            <tbody>
+              <tr v-for="purchase in account.purchases" :key="purchase.id" class="bg-white dark:bg-gray-900 dark:border-gray-700" :class="{'border-b': purchase.id !== account.purchases[Object.keys(account.purchases)[Object.keys(account.purchases).length - 1]].id}">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {{ purchase.stock.code }}
+                </th>
+                <td class="px-6 py-4">
+                  {{ purchase.stock.name }}
+                </td>
+                <td class="px-6 py-4">
+                  {{ purchase.purchase_price }}
+                </td>
+                <td class="px-6 py-4">
+                  {{ timeSince(purchase.purchased_at) }}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </template>
+      </AppCard>
     </div>
     <div
       v-else
