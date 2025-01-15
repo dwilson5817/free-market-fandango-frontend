f2a98508c879340cc820c0bff044cd21d57d526d
Dylan Wilson <dwilson5817@gmail.com>
2025-01-15 23:43:27 +0000

    chore(shadcn-ui): add dark mode toggle
    Create toggle and theme provider from shadcn/ui documentation.

--- src/App.tsx
+++ src/App.tsx
@@ -3,43 +3,48 @@ import { Label } from "@/components/ui/label.tsx";
 import { Input } from "@/components/ui/input.tsx";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select.tsx";
 import { Button } from "@/components/ui/button.tsx";
+import { ThemeProvider } from "@/components/theme-provider.tsx";
+import { ModeToggle } from "@/components/mode-toggle.tsx";
 
 function App() {
   return (
-      <Card className="w-[350px]">
-          <CardHeader>
-              <CardTitle>Create project</CardTitle>
-              <CardDescription>Deploy your new project in one-click.</CardDescription>
-          </CardHeader>
-          <CardContent>
-              <form>
-                  <div className="grid w-full items-center gap-4">
-                      <div className="flex flex-col space-y-1.5">
-                          <Label htmlFor="name">Name</Label>
-                          <Input id="name" placeholder="Name of your project" />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <ModeToggle />
+          <Card className="w-[350px]">
+              <CardHeader>
+                  <CardTitle>Create project</CardTitle>
+                  <CardDescription>Deploy your new project in one-click.</CardDescription>
+              </CardHeader>
+              <CardContent>
+                  <form>
+                      <div className="grid w-full items-center gap-4">
+                          <div className="flex flex-col space-y-1.5">
+                              <Label htmlFor="name">Name</Label>
+                              <Input id="name" placeholder="Name of your project" />
+                          </div>
+                          <div className="flex flex-col space-y-1.5">
+                              <Label htmlFor="framework">Framework</Label>
+                              <Select>
+                                  <SelectTrigger id="framework">
+                                      <SelectValue placeholder="Select" />
+                                  </SelectTrigger>
+                                  <SelectContent position="popper">
+                                      <SelectItem value="next">Next.js</SelectItem>
+                                      <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                                      <SelectItem value="astro">Astro</SelectItem>
+                                      <SelectItem value="nuxt">Nuxt.js</SelectItem>
+                                  </SelectContent>
+                              </Select>
+                          </div>
                       </div>
-                      <div className="flex flex-col space-y-1.5">
-                          <Label htmlFor="framework">Framework</Label>
-                          <Select>
-                              <SelectTrigger id="framework">
-                                  <SelectValue placeholder="Select" />
-                              </SelectTrigger>
-                              <SelectContent position="popper">
-                                  <SelectItem value="next">Next.js</SelectItem>
-                                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                                  <SelectItem value="astro">Astro</SelectItem>
-                                  <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                              </SelectContent>
-                          </Select>
-                      </div>
-                  </div>
-              </form>
-          </CardContent>
-          <CardFooter className="flex justify-between">
-              <Button variant="outline">Cancel</Button>
-              <Button>Deploy</Button>
-          </CardFooter>
-      </Card>
+                  </form>
+              </CardContent>
+              <CardFooter className="flex justify-between">
+                  <Button variant="outline">Cancel</Button>
+                  <Button>Deploy</Button>
+              </CardFooter>
+          </Card>
+      </ThemeProvider>
   )
 }
 
